eb24fcaaf061c14ee8cd004b2dc238d9adfd3be2
Bill Piel <bill@billpiel.com>
2016-10-19 21:49:05 -0400

    ...

--- src/contrived_example/inner_workings.clj
+++ src/contrived_example/inner_workings.clj
@@ -4,7 +4,7 @@
   {:quarter 0.25
    :dime 0.10
    :nickel 0.05
-   :penny 0.01})
+   :penny 1})
 
 (defn- calc-coin-value
   [coins]
--- src/contrived_example/sayid.clj
+++ src/contrived_example/sayid.clj
@@ -2,4 +2,7 @@
   (:require [com.billpiel.sayid.nrepl-middleware :as smw]
             [com.billpiel.sayid.view :as sv]))
 
-(smw/register-view! :lib-decide (sv/pred-sel-pairs->view [[[:name #"decide"] {:return true :selects {:name [:arg-map 'name]}}]]))
+(smw/register-view! :lib-decide
+                    (sv/pred-sel-pairs->view [[[:name #"decide"]
+                                               {:return true
+                                                :selects {:name [:arg-map 'name]}}]]))
